5540e81bff7921abf2aac285965b2692f7203a3c
Vidya Muniraju <vidya.muniraju@csu.fullerton.edu>
2020-10-23 23:58:23 -0500

    Added data security and validation

--- index.js
+++ index.js
@@ -12,10 +12,16 @@ const mongoose = require('mongoose'),
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect("mongodb://localhost:27017/myFlixDB", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
+// mongoose.connect("mongodb://localhost:27017/myFlixDB", {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// });
+
+mongoose.connect( process.env.CONNECTION_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 const app = express();
 
